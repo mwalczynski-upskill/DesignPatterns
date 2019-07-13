71bf6c30aabaae2d16a346376e09fc5eabdfbf83
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-07-13 13:41:24 +0200

    Dispose pattern

---
+++ Dispose/SealedClass.cs
@@ -0,0 +1,20 @@
+﻿namespace Dispose
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public sealed class SealedClass : IDisposable
+    {
+        private readonly IntPtr _memoryHandle;
+
+        public SealedClass()
+        {
+            _memoryHandle = Marshal.AllocCoTaskMem(1);
+        }
+
+        public void Dispose()
+        {
+            Marshal.FreeCoTaskMem(_memoryHandle);
+        }
+    }
+}
---
+++ Dispose/UnsealedBaseClass.cs
@@ -0,0 +1,38 @@
+﻿namespace Dispose
+{
+    using System;
+
+    public class UnsealedBaseClass : IDisposable
+    {
+        // Flag: Has Dispose already been called?
+        private bool _disposed = false;
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                // get rid of managed resources:
+                // * objects that implement IDisposable, eg. SafeHandles,
+                // * objects that consume large amounts of memory or consume scarce resources
+            }
+
+            // get rid of unmanaged resources
+            _disposed = true;
+        }
+
+        ~UnsealedBaseClass()
+        {
+            Dispose(false);
+        }
+    }
+}
